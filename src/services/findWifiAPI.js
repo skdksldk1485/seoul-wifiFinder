50d71f629c1f8a6b2a8123aba70cd5040c22b746
skdksldk1485 <skdksldk1485@naver.com>
2021-09-26 01:14:58 +0900

    Test to deploy

--- src/services/findWifiAPI.js
+++ src/services/findWifiAPI.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
   export const findWifiLocations = async (start = 1, end = 1000) => {
-    const url = `//openapi.seoul.go.kr:8088/${process.env.REACT_APP_PUBLIC_WIFI_API_KEY}/json/TbPublicWifiInfo_GN/${start}/${end}/`;
+    const url = `http://openapi.seoul.go.kr:8088/${process.env.REACT_APP_PUBLIC_WIFI_API_KEY}/json/TbPublicWifiInfo_GN/${start}/${end}/`;
     const response = await axios.get(url);
     const locations = response.data.TbPublicWifiInfo_GN.row;
     const totalCount = response.data.TbPublicWifiInfo_GN.list_total_count;
